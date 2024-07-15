7807056b99741c96e0134ab8c819bd7fc7556791
Yar Khan <72460103+YarKhan02@users.noreply.github.com>
2024-07-15 00:22:45 +0000

    connected production (rds) database

--- .devcontainer/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set the IP_GITHUB environment variable
-export IP_GITHUB=$(curl -s ifconfig.me)
-echo "IP_GITHUB=$IP_GITHUB" >> /etc/environment
-
-# Run the rds-update-sg-rule script
-bash $THEIA_WORKSPACE_ROOT/backend-flask/bin/rds-update-sg-rule
