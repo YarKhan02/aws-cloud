d10b5db4f293196389f248a84de66816736ce7bd
Yar Khan <72460103+YarKhan02@users.noreply.github.com>
2024-07-31 17:20:03 +0000

    refactor the db library

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool, query_wrap_array
+from lib import db
 
 tracer = trace.get_tracer("home.activities")
 
@@ -12,7 +12,7 @@ class HomeActivities:
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
 
-      sql = query_wrap_array("""
+      results = db.query_array_json("""
       SELECT
         activities.uuid,
         users.display_name,
@@ -29,10 +29,4 @@ class HomeActivities:
       ORDER BY activities.created_at DESC
       """)
 
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchone()
-      return json[0]
+      return results
